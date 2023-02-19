7805de028717de60bab557da9e7ee162f2297ba3
Fabien Piuzzi <fabien@reefab.net>
2023-02-19 16:39:40 +0100

    coding style fixes

--- api.py
+++ api.py
@@ -3,27 +3,27 @@ import json
 from urllib.request import urlopen, Request
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
-class APIHandler(BaseHTTPRequestHandler):
 
+class APIHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         host = self.path[1:]
 
         status = self.get_status(host)
 
         self.send_response(200)
-        self.send_header('Content-Type', 'application/json')
+        self.send_header("Content-Type", "application/json")
         self.end_headers()
-        self.wfile.write(json.dumps({'Status': status}).encode())
+        self.wfile.write(json.dumps({"Status": status}).encode())
 
     def get_status(self, host):
-        url = f'http://{host}:7000/info'
-        httprequest = Request(url)
+        httprequest = Request(f"http://{host}:7000/info")
 
         with urlopen(httprequest) as response:
             content = response.read()
 
         data = plistlib.loads(content)
-        return data['statusFlags']> 2000
+        return data["statusFlags"] > 2000
 
-httpd = HTTPServer(('0.0.0.0', 8000), APIHandler)
+
+httpd = HTTPServer(("0.0.0.0", 8000), APIHandler)
 httpd.serve_forever()
