46ba7aad2810910c28279455ed5175a427e7149d
Oskar Joelsson <oskar.joelsson@gmail.com>
2023-05-05 22:06:56 +0200

    fixes, needs to test

--- api.py
+++ api.py
@@ -17,30 +17,27 @@ class APIHandler(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(json.dumps({"Status": "OK"}).encode())
         else:
-            status = self.get_status(host)
-            self.send_response(200)
+            (status, statusCode) = self.get_status(host)
+            self.send_response(statusCode)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps({"Status": status}).encode())
 
-    def get_status(self, host) -> bool:
+    def get_status(self, host):
         try:
             with urlopen(f"http://{host}:7000/info", timeout=60) as response:
                 content = response.read()
+                data = plistlib.loads(content)
+                return (data["statusFlags"] > 2000, response.status)
         except HTTPError as error:
             print(error.status, error.reason)
-            return False
+            return (False, error.status)
         except URLError as error:
             print(error.reason)
-            return False
+            return (False, "500")
         except TimeoutError:
             print("Request timed out")
-            return False
-
-
-        data = plistlib.loads(content)
-        return data["statusFlags"] > 2000
-
+            return (False, "408")
 
 httpd = ThreadingHTTPServer(("0.0.0.0", 8000), APIHandler)
 httpd.serve_forever()
